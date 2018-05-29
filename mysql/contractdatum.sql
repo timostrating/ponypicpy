5318d62d3c847b3e90f0f43fcb47c6ba0c37676d
Timo Strating <timostrating@users.noreply.github.com>
2018-05-29 21:41:31 +0200

    Update contractdatum.sql

--- mysql/contractdatum.sql
+++ mysql/contractdatum.sql
@@ -1,7 +1,10 @@
 SELECT 
-	datum, 
+	MIN(datum) as datum, 
 	HOUR(datum) AS uur, 
-    DAYNAME(datum) AS dag,
-    WEEK(datum) AS week,
-    DAY(datum) AS dagnummer
-FROM ponydb.aanmeldingen;
+	DAYNAME(datum) AS dag,
+	WEEK(datum) AS weeknummer,
+	DAY(datum) AS dagnummer
+	count(*) as aantal_uitingen,
+FROM ponydb.aanmeldingen
+GROUP BY datum
+ORDER BY aantal DESC;
