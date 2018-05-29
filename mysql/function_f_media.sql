496cbbf33c5176f126101b9de17f45d3545762b2
Timo Strating <timostrating@users.noreply.github.com>
2018-05-29 21:40:34 +0200

    Update function_f_media.sql

--- mysql/function_f_media.sql
+++ mysql/function_f_media.sql
@@ -1,3 +1,4 @@
+-- OUR VESION
 DELIMITER $$
 DROP FUNCTION IF EXISTS f_media;
 CREATE FUNCTION f_media(datum DATETIME) RETURNS INT
@@ -9,3 +10,20 @@ BEGIN
         WHERE mediauitingen.datum = DATE(datum);
 	RETURN found;
 END$$
+DELIMITER ;
+
+
+-- EXCERCISE VERSION
+DELIMITER $$
+DROP FUNCTION IF EXISTS f_media;
+CREATE FUNCTION f_media(weeknumer int, dagnummer int, media varchar(45)) RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE found INT;
+		SELECT COUNT(*) INTO found 
+        FROM mediauitingen 
+        WHERE mediauitingen.datum = DATE_ADD(MAKEDATE(2011, weeknumer), INTERVAL dagnummer DAY) 
+			AND mediauitingen.categorie LIKE CONCAT('%',media,'%');
+	RETURN found;
+END$$
+DELIMITER ;
