5120b9d035c7fdebdb73863cc40e62f0c53ba913
hilkojj <hilkojj@outlook.com>
2018-06-22 11:32:07 +0200

    titles

--- ponypicnotpy/src/app/app.component.ts
+++ ponypicnotpy/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { routerAnimation } from './animations/routerAnimation';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
 
 @Component({
     selector: 'app-root',
@@ -8,6 +14,33 @@ import { routerAnimation } from './animations/routerAnimation';
     animations: [routerAnimation]
 })
 export class AppComponent {
+
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
+        private title: Title
+    ) {
+
+        this.subscribeToNavEnd();
+
+    }
+
+    private subscribeToNavEnd() {
+
+        this.router.events
+            .filter((event) => event instanceof NavigationEnd)
+            .map(() => this.activatedRoute)
+            .map((route) => {
+                while (route.firstChild) route = route.firstChild;
+                return route;
+            })
+            .filter((route) => route.outlet === 'primary')
+            .mergeMap((route) => route.data)
+            .subscribe(data => {
+                if ("title" in data)
+                    this.title.setTitle(data["title"]);
+            });
+    }
 
     getState(outlet): any {
         return outlet.activatedRouteData.index ? "index" : outlet
