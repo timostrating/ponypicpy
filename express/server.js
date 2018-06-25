90d9a2f2d02fba658241b8c1fd8aac3e68a8b848
hilkojj <hilkojj@outlook.com>
2018-06-25 20:19:28 +0200

    9090

--- express/server.js
+++ express/server.js
@@ -31,4 +31,4 @@ app.get("/", (req, res, next) => {
 app.use(minify());
 app.use("/", express.static(__dirname + "/web_app"));
 app.use("*", (req, res) => res.sendFile(__dirname + "/web_app/index.html"));
-app.listen(9090, () => console.log("Ponypic listening on port 8080"));
+app.listen(9090, () => console.log("Ponypic listening on port 9090"));
